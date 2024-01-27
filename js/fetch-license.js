a0649be0feb49132c5b829d17b997231edd28570
Mirza Akbar Ail <mirza.akbarali.59@gmail.com>
2024-01-27 14:58:14 +0100

    pages in separate folder and footer

--- js/fetch-license.js
+++ js/fetch-license.js
@@ -1,10 +1,10 @@
 // fetch-sidebar.js
 
 // Use fetch to load sidebar content
-fetch('../sidebar.html')
+fetch('../LICENSE.html')
     .then(response => response.text())
     .then(data => {
         // Insert the fetched content into the sidebar container
-        document.getElementById('sidebar-container').innerHTML = data;
+        document.getElementById('license').innerHTML = data;
     })
     .catch(error => console.error('Error fetching sidebar content:', error));
